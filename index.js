028c841bb9c5c37878838c4379d497f842e37bb4
bhayanisumit <bhayanisumit@gmail.com>
2019-11-14 02:57:55 +0530

    changes

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+// var http = require('http').createServer(app);
+const express = require('express');
+const fs = require('fs');
+const https = require('https');
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/invitee.ca/privkey.pem','utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/invitee.ca/cert.pem','utf8');
+const credentials = {
+        key: privateKey,
+        cert: certificate
+};
+const app =express();
+const httpsServer = https.createServer(credentials, app);
+var io = require('socket.io')(httpsServer);
+
+
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -12,6 +24,6 @@ io.on('connection', function(socket){
     });
   });
 
-http.listen(3000, function(){
+  httpsServer.listen(3000, function(){
   console.log('listening on *:3000');
 });
